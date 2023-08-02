5069d8bd3a0dbd46784cc2562feb3115149ed95c
Vita1ius <vitalikbalushak@gmail.com>
2023-08-02 16:52:51 +0300

    delete folder build

--- build/link.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from './prisma.service';
-import { Link, Prisma } from '@prisma/client';
-export declare class LinkService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createUser(data: Prisma.LinkCreateInput): Promise<Link>;
-}
